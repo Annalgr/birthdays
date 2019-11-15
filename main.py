554f54fc394699cd93a73d912e0596ab6261d7dd
Annalgr <anna.lagrasta@student.h-farm.com>
2019-11-15 20:15:53 +0100

    Add sys.argv to main

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 #! /usr/bin/env python3
 
 from mypackage.birthdays import return_birthday
+import sys
 
-return_birthday('Albert Einstein')
-return_birthday('Alan Turing')
+if len(sys.argv) > 1:
+    argument = sys.argv[1]
+else:
+    print("Give me an argument to print")
+    exit()
+
+return_birthday(argument)
