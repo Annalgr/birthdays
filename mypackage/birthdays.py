438c5a68ace0f372f81fff618b4eb11595162b8f
FrancescaShell <francesca.griggio@student.h-farm.com>
2019-11-15 19:30:25 +0100

    document the package

--- mypackage/birthdays.py
+++ mypackage/birthdays.py
@@ -1,3 +1,7 @@
+'''
+These module contains a set of function to find the birthday date of the person you are looking for.
+'''
+
 birthdays = {
     'Albert Einstein': '03/14/1879',
     'Benjamin Franklin': '01/17/1706',
@@ -6,11 +10,13 @@ birthdays = {
     'Rowan Atkinson': '01/6/1955'}
 
 def print_birthdays():
+    '''For each element in the dictionary return print the element.'''
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
     for name in birthdays:
         print(name)
 
 def return_birthday(name):
+    '''The function returns the birthday day if the person is preset in the dictionary.'''
     if name in birthdays:
         print('{}\'s birthday is {}.'.format(name, birthdays[name]))
     else:
